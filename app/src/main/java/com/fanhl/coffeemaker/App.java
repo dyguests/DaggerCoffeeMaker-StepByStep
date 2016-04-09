f2fb9a10174c2a46fd489e68da8f4d0e2247c7ca
fanhl <Dyguest0315>
2016-04-09 22:51:10 +0800

    2.2.1.2.建立AppComponent(@Inject和@Module的桥梁)

---
+++ app/src/main/java/com/fanhl/coffeemaker/App.java
@@ -0,0 +1,14 @@
+package com.fanhl.coffeemaker;
+
+import android.app.Application;
+
+/**
+ * Created by fanhl on 16/4/9.
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/fanhl/coffeemaker/AppComponent.java
@@ -0,0 +1,13 @@
+package com.fanhl.coffeemaker;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by fanhl on 16/4/9.
+ */
+@Singleton//2.2.1.2.2
+@Component//2.2.1.2.1
+public interface AppComponent {
+}
