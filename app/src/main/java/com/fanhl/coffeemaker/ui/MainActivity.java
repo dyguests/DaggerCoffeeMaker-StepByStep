e19c339f662821ce5b8900b6d255418216cf9149
fanhl <Dyguest0315>
2016-04-09 21:12:37 +0800

    新建Android Studio项目

--- app/src/main/java/com/fanhl/coffeemaker/ui/MainActivity.java
+++ app/src/main/java/com/fanhl/coffeemaker/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.fanhl.coffeemaker;
+package com.fanhl.coffeemaker.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.fanhl.coffeemaker.R;
 
 public class MainActivity extends AppCompatActivity {
 
