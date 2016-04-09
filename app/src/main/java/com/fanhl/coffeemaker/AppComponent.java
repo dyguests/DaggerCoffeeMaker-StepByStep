1f49c62c67d61f648fb2a016d76024dc5cfabc43
fanhl <Dyguest0315>
2016-04-10 00:20:48 +0800

    2.2.3.4.连接@Inject和@Module

--- app/src/main/java/com/fanhl/coffeemaker/AppComponent.java
+++ app/src/main/java/com/fanhl/coffeemaker/AppComponent.java
@@ -1,5 +1,6 @@
 package com.fanhl.coffeemaker;
 
+import com.fanhl.coffeemaker.machine.MachineModule;
 import com.fanhl.coffeemaker.ui.MainActivity;
 
 import javax.inject.Singleton;
@@ -10,7 +11,7 @@ import dagger.Component;
  * Created by fanhl on 16/4/9.
  */
 @Singleton//2.2.1.2.2
-@Component//2.2.1.2.1
+@Component(modules = {MachineModule.class})//2.2.3.4.1
 public interface AppComponent {
     void inject(MainActivity activity);//2.2.2.1.1
 }
