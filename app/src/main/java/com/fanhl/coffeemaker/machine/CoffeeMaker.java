8fd7eaa359acb7bd843e6137e9d2c5d530d0b2d9
fanhl <Dyguest0315>
2016-04-09 22:03:17 +0800

    2.1.安装咖啡机

---
+++ app/src/main/java/com/fanhl/coffeemaker/machine/CoffeeMaker.java
@@ -0,0 +1,13 @@
+package com.fanhl.coffeemaker.machine;
+
+/**
+ * Created by fanhl on 16/4/9.
+ */
+public class CoffeeMaker {
+
+    public String brew() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[_]P coffee! [_]P\n");
+        return sb.toString();
+    }
+}
