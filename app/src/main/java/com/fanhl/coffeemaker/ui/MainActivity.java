b0ae15af169a7a37b41eb31eca468e03571bbe78
fanhl <Dyguest0315>
2016-04-09 21:45:48 +0800

    1.3.初始化布局文件.

--- app/src/main/java/com/fanhl/coffeemaker/ui/MainActivity.java
+++ app/src/main/java/com/fanhl/coffeemaker/ui/MainActivity.java
@@ -1,15 +1,28 @@
 package com.fanhl.coffeemaker.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.fanhl.coffeemaker.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.result)
+    TextView mResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.brew)
+    void brew() {
     }
 }
